f58c53f9ddc944849291e9d7ec839962a496be76
Will Meek <william.meek@puppet.com>
2017-12-01 11:01:03 +0000

    Unit test for default dev dependencies

--- spec/unit/puppet-module-gems/dependencies_parser_spec.rb
+++ spec/unit/puppet-module-gems/dependencies_parser_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
+include PuppetModuleGems::Constants
+
 describe PuppetModuleGems::DependenciesParser do
   describe '#build_gem_matrix' do
 
@@ -63,5 +65,32 @@ describe PuppetModuleGems::DependenciesParser do
       end
 
     end
+
+    describe 'default dependencies'
+    it 'should import data from default dependencies file for dev' do
+      result = PuppetModuleGems::DependenciesParser.build_gem_matrix(DEPENDENCIES_FILE)
+      expect(result).to be_a Hash
+      ['posix-dev-r2.1', 'posix-dev-r2.3', 'posix-dev-r2.4', 'win-dev-r2.1', 'win-dev-r2.3', 'win-dev-r2.4'].each do |key|
+        expect(result[key]).to include({'gem'=>'beaker-task_helper', 'version'=>['>= 1.1.0', '< 2.0.0']})
+        expect(result[key]).to include({'gem'=>'gettext-setup', 'version'=>'~> 0.26'})
+        expect(result[key]).to include({'gem'=>'metadata-json-lint', 'version'=>['>= 2.0.2', '< 3.0.0']})
+        expect(result[key]).to include({'gem'=>'mocha', 'version'=>['>= 1.0.0', '< 1.2.0']})
+        expect(result[key]).to include({'gem'=>'parallel_tests', 'version'=>['>= 2.14.1', '< 2.14.3']})
+        expect(result[key]).to include({'gem'=>'pry', 'version'=>'~> 0.10.4'})
+        expect(result[key]).to include({'gem'=>'puppet-lint', 'version'=>['>= 2.3.0', '< 3.0.0']})
+        expect(result[key]).to include({'gem'=>'puppet_pot_generator', 'version'=>'~> 1.0'})
+        expect(result[key]).to include({'gem'=>'puppet-syntax', 'version'=>['>= 2.4.1', '< 3.0.0']})
+        expect(result[key]).to include({'gem'=>'puppetlabs_spec_helper', 'version'=>['>= 2.3.1', '< 3.0.0']})
+        expect(result[key]).to include({'gem'=>'rainbow', 'version'=>['>= 2.0.0', '< 2.2.0']})
+        expect(result[key]).to include({'gem'=>'rspec-puppet', 'version'=>['>= 2.3.2', '< 3.0.0']})
+        expect(result[key]).to include({'gem'=>'rspec-puppet-facts', 'version'=>'~> 1.8'})
+        expect(result[key]).to include({'gem'=>'rubocop', 'version'=>'~> 0.49'})
+        expect(result[key]).to include({'gem'=>'rubocop-i18n', 'version'=>'~> 1.0'})
+        expect(result[key]).to include({'gem'=>'rubocop-rspec', 'version'=>'~> 1.15'})
+        expect(result[key]).to include({'gem'=>'rspec_junit_formatter', 'version'=>'~> 0.2'})
+        expect(result[key]).to include({'gem'=>'specinfra', 'version'=>'2.67.3'})
+      end
+    end
   end
 end
+
